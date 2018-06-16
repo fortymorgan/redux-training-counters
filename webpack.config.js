a2681d27506044528b9db09d2e0fd729ca316cdb
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-16 18:18:13 +0300

    Remove entry and output from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,3 @@
-const path = require('path');
- 
 module.exports = {
-  entry: './js/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
   devtool: 'eval',
 };
