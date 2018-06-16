62cf812e9af64bbe4bcd9acc913bad32cf98ee35
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-16 18:47:26 +0300

    Split render into functions

--- src/render.js
+++ src/render.js
@@ -1,5 +1,42 @@
 const getElement = (prefix, id) => document.getElementById(`${prefix}-${id}`);
 
+const createCounterHtml = (counter, container) => {
+  const { id, value } = counter;
+
+  const counterDiv = document.createElement('div');
+  counterDiv.id = `counter-${id}`;
+
+  counterDiv.innerHTML = `<span id="value-${id}">${value}</span>
+  <button id="dec-${id}">-</button>
+  <button id="inc-${id}">+</button>
+  <button id="del-${id}">x</button>`;
+
+  container.append(counterDiv);
+};
+
+const addHandlers = (counter, store) => {
+  const { id } = counter;
+
+  const delButton = getElement('del', id);
+  const decButton = getElement('dec', id);
+  const incButton = getElement('inc', id);
+
+  delButton.addEventListener('click', () => {
+    const action = { type: 'COUNTER_REMOVE', id };
+    store.dispatch(action);
+  });
+
+  decButton.addEventListener('click', () => {
+    const action = { type: 'DECREMENT', id };
+    store.dispatch(action);
+  });
+
+  incButton.addEventListener('click', () => {
+    const action = { type: 'INCREMENT', id };
+    store.dispatch(action);
+  });
+}
+
 module.exports = (store) => {
   const state = store.getState();
   
@@ -20,35 +57,7 @@ module.exports = (store) => {
   console.log(state);
 
   state.counters.forEach(element => {
-    const { id, value } = element;
-
-    const counter = document.createElement('div');
-    counter.id = `counter-${id}`;
-
-    counter.innerHTML = `<span id="value-${id}">${value}</span>
-    <button id="dec-${id}">-</button>
-    <button id="inc-${id}">+</button>
-    <button id="del-${id}">x</button>`;
-
-    countersDiv.appendChild(counter);
-
-    const delButton = getElement('del', id);
-    const decButton = getElement('dec', id);
-    const incButton = getElement('inc', id);
-
-    delButton.addEventListener('click', () => {
-      const action = { type: 'COUNTER_REMOVE', id };
-      store.dispatch(action);
-    });
-
-    decButton.addEventListener('click', () => {
-      const action = { type: 'DECREMENT', id };
-      store.dispatch(action);
-    });
-
-    incButton.addEventListener('click', () => {
-      const action = { type: 'INCREMENT', id };
-      store.dispatch(action);
-    });
+    createCounterHtml(element, countersDiv);
+    addHandlers(element, store);
   });
 }
