81c3952eed3f3fe2e3ddc4dd0b5f2481dca8390a
Forty Morgan <dimazhmudikov@gmail.com>
2018-06-29 18:56:33 +0300

    Add redux dev tool

--- src/app.js
+++ src/app.js
@@ -5,7 +5,15 @@ const render = require('./render');
 module.exports = () => {
   const combinedReducers = redux.combineReducers(reducers);
 
-  const store = redux.createStore(combinedReducers);
+  // const store = redux.createStore(combinedReducers);
+
+  const ext = window.__REDUX_DEVTOOLS_EXTENSION__;
+  const devtoolMiddleware = ext && ext();
+
+  const store = redux.createStore(
+    combinedReducers,
+    devtoolMiddleware
+  )
   
   const renderWithStore = () => render(store);
   
